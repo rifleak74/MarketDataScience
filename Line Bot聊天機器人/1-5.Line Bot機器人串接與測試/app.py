6e1472df1f1b20aff38a6d5f4023d0bfb9fc0154
Auriclee <91070121+AuricLee@users.noreply.github.com>
2021-09-22 12:14:11 +0800

    Update app.py

--- Line Bot聊天機器人/1-5.Line Bot機器人串接與測試/app.py
+++ Line Bot聊天機器人/1-5.Line Bot機器人串接與測試/app.py
@@ -50,9 +50,21 @@ def callback():
 #訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+ def handle_message(event): message = text=event.message.text 
+         if re.match('給我holo色圖',message): 
+                flex_message = TextSendMessage(text='請選擇你要到的圖片', 
+                                       quick_reply=QuickReply(items=[ 
+                                           QuickReplyButton(action=MessageAction(label="486", text="婆！")),
+                                           QuickReplyButton(action=MessageAction(label="446", text="婆！")), 
+                                           QuickReplyButton(action=MessageAction(label="FBK", text="婆！")), 
+                                           QuickReplyButton(action=MessageAction(label="Tay", text="甲甲！！")), 
+                                           QuickReplyButton(action=MessageAction(label="Aqua", text="婆！")), 
+                                           QuickReplyButton(action=MessageAction(label="nene", text="婆！")),
+                                           QuickReplyButton(action=MessageAction(label="gura", text="婆！")), 
+                                           QuickReplyButton(action=MessageAction(label="色祭", text="婆！")), 
+                                           QuickReplyButton(action=MessageAction(label="yagoo", text="暫無此類")) 
+                                       ])) 
+                line_bot_api.reply_message(event.reply_token, flex_message)
 
 #主程式
 import os
