5a376b22d7c1441b277dd7c805e4ae73adf52489
Ivan Yang <ivanyang0606@gmail.com>
2022-04-22 23:48:14 +0800

    更新紀錄 2022/4/22
    Yahoo舊版網頁全面捨棄，因此修正找不到的標籤

--- 行銷人轉職爬蟲王實戰｜5大社群＋2大電商/1-3.Html爬蟲Get教學－Yahoo/1-3.Html爬蟲Get教學－Yahoo.py
+++ 行銷人轉職爬蟲王實戰｜5大社群＋2大電商/1-3.Html爬蟲Get教學－Yahoo/1-3.Html爬蟲Get教學－Yahoo.py
@@ -8,6 +8,9 @@ Created on Wed Apr 14 16:09:15 2021
 
 第一章 爬蟲基本訓練
 Html爬蟲教學Get教學－Yahoo
+
+更新紀錄
+2022/4/22：Yahoo舊版網頁全面捨棄，因此修正找不到的標籤
 """
 import requests
 from bs4 import BeautifulSoup
@@ -20,13 +23,13 @@ list_req = requests.get(url)
 #將整個網站的程式碼爬下來
 soup = BeautifulSoup(list_req.content, "html.parser")
 #找到th這個標籤
-getstock= soup.find('th').text #只抓到第一個<th>
+getstock= soup.find('li').text #只抓到第一個<li>
 print(getstock)
-getstock= soup.findAll('th') #抓到很多個th
+getstock= soup.findAll('li') #抓到很多個li
 #找到b這個標籤
-getstock= soup.find('b').text #抓到<b>
+getstock= soup.find('a').text #抓到<b>
 print(getstock)
 #找到table這個標籤
-soup.find('table') #只找到一個
-soup.find_all('table') #找到網頁內所有的table
-soup.find_all('table', {'border':'2'}) #特別指定'border':'2'
+soup.find('li') #只找到一個
+soup.find_all('li') #找到網頁內所有的li
+soup.find_all('li', {'id':'root_3'}) #特別指定'id':'root_3'
