da9bc9125ce5b97ba51360460bb70c36bbc8a94a
Ivan Yang <ivanyang0606@gmail.com>
2022-09-23 11:54:59 +0800

    selenium套件更新
    將套件更新到4.4.3版本，因此寫法全部都更新過

--- 行銷人轉職爬蟲王實戰｜5大社群＋2大電商/7-1.FB登入/7-1.FB登入.py
+++ 行銷人轉職爬蟲王實戰｜5大社群＋2大電商/7-1.FB登入/7-1.FB登入.py
@@ -9,20 +9,18 @@ Created on Fri May  7 17:02:59 2021
 第七章 FB自動發社團發文小幫手
 FB登入
 """
-from selenium.webdriver import DesiredCapabilities
+# selenium，2022/9/17 將套件更新到4.4.3版本，因此寫法全部都更新過
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 import time
     
 useEmail = '帳號'
 usePass = '密碼'
 
-# 設定基本參數
-desired_capabilities = DesiredCapabilities.PHANTOMJS.copy()
-#此處必須換成自己電腦的User-Agent
-desired_capabilities['phantomjs.page.customHeaders.User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
-
-# PhantomJS driver 路徑 似乎只能絕對路徑
-driver = webdriver.PhantomJS(executable_path = 'phantomjs', desired_capabilities=desired_capabilities)
+# 自動下載ChromeDriver
+service = ChromeService(executable_path=ChromeDriverManager().install())
 
 # 關閉通知提醒
 chrome_options = webdriver.ChromeOptions()
@@ -30,14 +28,16 @@ prefs = {"profile.default_content_setting_values.notifications" : 2}
 chrome_options.add_experimental_option("prefs",prefs)
 
 # 開啟瀏覽器
-driver = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
+driver = webdriver.Chrome(service=service, chrome_options=chrome_options)
+time.sleep(5)
 
+# 開啟網頁
 driver.get("http://www.facebook.com")
 time.sleep(3)
 assert "Facebook" in driver.title
 
-driver.find_element_by_id('email').send_keys(useEmail) # 輸入帳號
+driver.find_element(by=By.ID, value='email').send_keys(useEmail) # 輸入帳號
 time.sleep(3)
-driver.find_element_by_id('pass').send_keys(usePass) # 輸入密碼
+driver.find_element(by=By.ID, value='pass').send_keys(usePass) # 輸入密碼
 time.sleep(1)
-driver.find_element_by_name('login').click() # 按下登入
+driver.find_element(by=By.NAME, value='login').click() # 按下登入
